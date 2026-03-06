00dc122db548d38ef7ad80aaecc8037d30c26927
Elmer <93677975+GWHodg@users.noreply.github.com>
2026-03-06 12:03:34 -0800

    idx, js & hx
    Mar 6/26

--- !gs63.js
+++ !gs63.js
@@ -3440,21 +3440,26 @@ else {
 				if (pwd === null) {
 			        return; //break out of the function early
 			    }
-			if (pwd == 'roentgen') {
+			if (pwd == 'r') {	//if (pwd == 'roentgen') {
 				//alert ("Will try to bring up selected menu item!");	
-				let mnuITEM = prompt("Enter MENU ITEM:\n\t1. Slider +/-\n\t2. SlideValsX\n\t3. NOTE"+
-								     "\n\t4. Matches\n\t5. Hx\n\t6. SAVED\n\t7. BOOK/bridge"+
-									 "\n\t8. coding *", "7");	
-				if ( mnuITEM == "7" ) { book() }
-				else if ( mnuITEM == "1" ) { showSLIDER() }
-				else if ( mnuITEM == "2" ) { clrSLDR() }
-				else if ( mnuITEM == "3" ) { note() }
-				else if ( mnuITEM == "4" ) { setLSsbfd() }
-				else if ( mnuITEM == "5" ) { history() }
-				else if ( mnuITEM == "6" ) { replace() }
+				let mnuITEM = prompt("Enter MENU ITEM:\n\t1. Hx\n\t2. NOTE"+
+								     "\n\t3. Matches\n\t4. BOOK/bridge"+
+									 "\n\t5. Slider +/-\n\t6. SlideValsX\n\t7. SAVED\n\t8. coding *"+
+									 "\n\t9. P5+/-\n\t10. Part Round\n\t11. Edit Stablefords", "1");	
+									 	//onclick="restrictTAB()"   Inputs+/  --->	  <!-- NB++ Feb/26 ** -->
+				if ( mnuITEM == "4" ) { book() }
+				else if ( mnuITEM == "5" ) { showSLIDER() }
+				else if ( mnuITEM == "6" ) { clrSLDR() }
+				else if ( mnuITEM == "2" ) { note() }
+				else if ( mnuITEM == "3" ) { setLSsbfd() }
+				else if ( mnuITEM == "1" ) { history() }
+				else if ( mnuITEM == "7" ) { replace() }
 				else if ( mnuITEM == "8" ) { codeHELP() }
+				else if ( mnuITEM == "9" ) { displayP5() }
+				else if ( mnuITEM == "10" ) { showPARTIAL() }
+				else if ( mnuITEM == "11" ) { editSP() }
 				else { return; }
 			}	
 			
 		}	
-	
+		
